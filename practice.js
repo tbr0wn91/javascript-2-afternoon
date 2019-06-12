cfac997d9e5c374ba41128aa0bf690612c8ff877
Thomas Brown <thomasbrown9101@gmail.com>
2019-06-12 15:08:47 -0700

    added solutions to problems

--- practice.js
+++ practice.js
@@ -15,6 +15,11 @@
 */
 
 //Code here
+let me = {
+  name: "Thomas",
+  age: 27,
+}
+alert(me.name)
 
 ////////// PROBLEM 2 //////////
 
@@ -24,20 +29,30 @@
 */
 
 //Code here
-
+let favoriteThings = {
+  band: "Architects",
+  food: "Pizza",
+  person: "Parents",
+  book: "Crime novels",
+  movie: "Forrest Gump",
+  holiday: "Christmas"
+}
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
 //Code here
-
+favoriteThings.car = "Ferrari"
+favoriteThings.brand = "458"
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
 //Code here
+favoriteThings.food = 'Chicken Nuggets'
+favoriteThings.book = 'Harry Potter'
 
 ////////// PROBLEM 3 //////////
 
@@ -50,13 +65,20 @@
 */
 
 //Code here
+let backPack = {}
+let item = 'firstPocket'
+backPack[item] = 'firstPocket'
+backPack['firstPocket'] = 'chapstick'
+backPack.color = 'black'
+
+
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
 //Code here
-
+// alert(backPack)
 /*
 You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
@@ -64,7 +86,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
-
+console.log(backPack)
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
@@ -85,7 +107,8 @@ var user2 = {
 */
 
 //Code Here
-
+user2.name = 'Bryan G. Smith'
+user2.email = 'bryan.smith@devmounta.in'
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
